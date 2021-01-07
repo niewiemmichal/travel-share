0e64d6960ce2ef0e88884f130e5959cea876c3eb
ClaudiaPolaczek <claudia.polaczek@gmail.com>
2021-01-07 23:38:56 +0100

    Add database model

---
+++ app/models.py
@@ -0,0 +1,42 @@
+# basic_api/models.py
+from django.db import models
+
+
+# DataFlair
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    friends = models.ManyToManyField("self", verbose_name="list of friends", symmetrical=False)
+
+    def __str__(self):
+        return self.name
+
+
+# DataFlair
+class Route(models.Model):
+    date = models.DateField()
+    combustion = models.FloatField()
+    fuel_price = models.FloatField()
+    route_length = models.FloatField()
+    route_participants = models.ManyToManyField(User, through='RouteMember', through_fields=('route', 'participant'))
+
+
+# DataFlair
+class RouteMember(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    participant = models.ForeignKey(User, on_delete=models.CASCADE)
+    route_price = models.FloatField()
+
+
+# DataFlair
+class Point(models.Model):
+    address = models.CharField(max_length=100)
+    getting_on_friends = models.ManyToManyField(User, verbose_name="list of friends getting on this point",
+                                                related_name='+')
+    getting_off_friends = models.ManyToManyField(User, verbose_name="list of friends getting off this point",
+                                                 related_name='+')
+
+    def __str__(self):
+        return self.address
