c75e839f2372bd88731cf458858450151f272814
Michal Granda <majcoch98@gmail.com>
2021-01-14 20:05:31 +0100

    Add new endpoint to url

--- app/router.py
+++ app/router.py
@@ -1,5 +1,7 @@
 from rest_framework import routers
-from .views import UsersViewset
+from .views import UsersViewset, RoutesViewset, RouteParticipantViewset
 
 router = routers.DefaultRouter()
-router.register('users', UsersViewset)
+router.register('users', UsersViewset),
+router.register('routes', RoutesViewset),
+router.register('route_participants', RouteParticipantViewset),
